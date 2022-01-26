9236fdc83bbf7a30f8d4ac8e8fa27d3257f93880
baimamboukar <baimamboukar@gmail.com>
2022-01-26 21:19:10 +0100

    main screen

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heldis/firebase_options.dart';
+import 'package:heldis/screens/screens.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
