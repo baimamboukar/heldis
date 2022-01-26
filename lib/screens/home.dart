4c175b090e45061f8544252f52f0da4122917628
baimamboukar <baimamboukar@gmail.com>
2022-01-26 21:35:10 +0100

    statistic view children

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -17,7 +17,36 @@ class Home extends StatelessWidget {
             SizedBox(
                 height: 200,
                 child: ListView.builder(itemBuilder: (context, index) {
-                  return Container();
+                  return Container(
+                    decoration: BoxDecoration(
+                      gradient: LinearGradient(colors: [
+                        Colors.orange,
+                        Colors.orange.withOpacity(0.6),
+                        Colors.purple
+                      ]),
+                      boxShadow: const [
+                        BoxShadow(color: Colors.purple, blurRadius: 10),
+                        BoxShadow(
+                            color: Colors.orange,
+                            blurRadius: 6,
+                            offset: Offset(100, 70))
+                      ],
+                    ),
+                    child: Stack(
+                      children: const [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: Icon(
+                            Ionicons.analytics,
+                            size: 100,
+                          ),
+                        ),
+                        Align(
+                            alignment: Alignment.center,
+                            child: Text("Students Analytics"))
+                      ],
+                    ),
+                  );
                 })),
           ],
         ),
