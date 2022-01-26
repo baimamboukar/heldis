9f0b9ff398e7af181fbceebf2455cf4985a6ac50
baimamboukar <baimamboukar@gmail.com>
2022-01-26 21:11:15 +0100

    materialapp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:heldis/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const ProviderScope(child: Heldis()));
+}
+
+class Heldis extends StatelessWidget {
+  const Heldis({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+        routeInformationParser: routeInformationParser,
+        routerDelegate: routerDelegate);
+  }
 }
