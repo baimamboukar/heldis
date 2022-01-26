32d32a84f6dd283708d11ebc58013b7875378dbe
baimamboukar <baimamboukar@gmail.com>
2022-01-26 21:44:07 +0100

    sizes added

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,39 +16,44 @@ class Home extends StatelessWidget {
           children: <Widget>[
             SizedBox(
                 height: 200,
-                child: ListView.builder(itemBuilder: (context, index) {
-                  return Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(28.0),
-                      gradient: LinearGradient(colors: [
-                        Colors.orange,
-                        Colors.orange.withOpacity(0.6),
-                        Colors.purple
-                      ]),
-                      boxShadow: const [
-                        BoxShadow(color: Colors.purple, blurRadius: 10),
-                        BoxShadow(
-                            color: Colors.orange,
-                            blurRadius: 6,
-                            offset: Offset(100, 70))
-                      ],
-                    ),
-                    child: Stack(
-                      children: const [
-                        Align(
-                          alignment: Alignment.topRight,
-                          child: Icon(
-                            Ionicons.analytics,
-                            size: 100,
-                          ),
+                child: ListView.builder(
+                    itemCount: 10,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        height: 180,
+                        width: 220,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(28.0),
+                          gradient: LinearGradient(colors: [
+                            Colors.orange,
+                            Colors.orange.withOpacity(0.6),
+                            Colors.purple
+                          ]),
+                          boxShadow: const [
+                            BoxShadow(color: Colors.purple, blurRadius: 10),
+                            BoxShadow(
+                                color: Colors.orange,
+                                blurRadius: 6,
+                                offset: Offset(100, 70))
+                          ],
                         ),
-                        Align(
-                            alignment: Alignment.center,
-                            child: Text("Students Analytics"))
-                      ],
-                    ),
-                  );
-                })),
+                        child: Stack(
+                          children: const [
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Icon(
+                                Ionicons.analytics,
+                                size: 100,
+                              ),
+                            ),
+                            Align(
+                                alignment: Alignment.center,
+                                child: Text("Students Analytics"))
+                          ],
+                        ),
+                      );
+                    })),
           ],
         ),
       ),
