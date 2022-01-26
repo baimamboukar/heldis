d7298b26eafb47c3a3c94cb588d687a003ca7c20
baimamboukar <baimamboukar@gmail.com>
2022-01-26 21:09:28 +0100

    added main and configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,8 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:heldis/firebase_options.dart';
 
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+}
