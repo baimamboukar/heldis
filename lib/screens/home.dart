af3e632b60fe380a984d2124917a66434adcb091
baimamboukar <baimamboukar@gmail.com>
2022-02-08 22:02:23 +0100

    home view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:heldis/widgets/block.dart';
 import 'package:ionicons/ionicons.dart';
 
 class Home extends StatelessWidget {
@@ -13,48 +14,16 @@ class Home extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-          children: <Widget>[
+          children: const <Widget>[
             SizedBox(
-                height: 200,
-                child: ListView.builder(
-                    itemCount: 10,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(14.0),
-                        child: Container(
-                          height: 180,
-                          width: 220,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(28.0),
-                            gradient: LinearGradient(colors: [
-                              Colors.orange.computeLuminance() > 0.5
-                                  ? Colors.orange.shade400
-                                  : Colors.orange.shade200,
-                              Colors.orange.withOpacity(0.6),
-                              Colors.purple
-                            ]),
-                            boxShadow: const [
-                              BoxShadow(color: Colors.purple, blurRadius: 10),
-                            ],
-                          ),
-                          child: Stack(
-                            children: const [
-                              Align(
-                                alignment: Alignment.topRight,
-                                child: Icon(
-                                  Ionicons.analytics,
-                                  size: 100,
-                                ),
-                              ),
-                              Align(
-                                  alignment: Alignment.center,
-                                  child: Text("Students Analytics"))
-                            ],
-                          ),
-                        ),
-                      );
-                    })),
+              height: 200,
+              child: Block(
+                title: "Classes",
+                desc: "Liste des classes",
+                icon: Icon(Ionicons.accessibility),
+                nextScreen: Home(),
+              ),
+            ),
           ],
         ),
       ),
