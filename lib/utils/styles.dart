2bd194df3f26c6bc20dbbf443d88dd963395b0d9
baimamboukar <baimamboukar@gmail.com>
2022-02-11 00:00:11 +0100

    styles file

---
+++ lib/utils/styles.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:heldis/utils/utils.dart';
+
+class Styles {
+  static TextStyle designText(
+          {required Color color, required double size, required bool bold}) =>
+      TextStyle(
+          fontSize: size,
+          color: color,
+          fontWeight: bold ? FontWeight.bold : FontWeight.normal);
+  static TextStyle header() => TextStyle(
+      fontSize: 22, color: Palette.primary, fontWeight: FontWeight.bold);
+}
