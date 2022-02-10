eee0716ad6a9fe6aff476a8be09513c8a776982a
baimamboukar <baimamboukar@gmail.com>
2022-02-10 23:20:16 +0100

    palette of colors

---
+++ lib/utils/palette.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static Color primary = Colors.indigo;
+  static Color secondary = Colors.grey;
+  static Color accent = Colors.pink;
+  static Color error = Colors.red;
+  static Color success = Colors.green;
+  static Color warning = Colors.orange;
+  static Color info = Colors.blue;
+  static Color dark = Colors.black26;
+}
