222a5655837bc7e42200895350917ca72fbc2fbd
baimamboukar <baimamboukar@gmail.com>
2022-02-11 17:57:39 +0100

    added appbar

--- lib/screens/parent_home.dart
+++ lib/screens/parent_home.dart
@@ -8,23 +8,31 @@ class ParentHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: AppBar(
+          title: const Text("Heldis"),
+          elevation: 0.0,
+        ),
         body: SingleChildScrollView(
-      physics: const BouncingScrollPhysics(),
-      child: Column(
-        children: [
-          Text(
-            "Cliquez sur le nom d'une personne pour voir ses informations",
-            style:
-                Styles.designText(color: Colors.black87, size: 20, bold: true),
+          physics: const BouncingScrollPhysics(),
+          child: Padding(
+            padding: const EdgeInsets.all(10.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 30),
+                Text(
+                  "Cliquez sur le nom d'une personne pour voir ses informations",
+                  style: Styles.designText(
+                      color: Colors.black87, size: 20, bold: true),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  "L'administration de l'ecole et vous, etes les seules personnes a avoir acces a ces informations",
+                  style: Styles.designText(
+                      color: Palette.dark, size: 18, bold: false),
+                ),
+              ],
+            ),
           ),
-          const SizedBox(height: 20),
-          Text(
-            "L'administration de l'ecole et vous, etes les seules personnes a avoir acces a ces informations",
-            style:
-                Styles.designText(color: Palette.dark, size: 18, bold: false),
-          ),
-        ],
-      ),
-    ));
+        ));
   }
 }
