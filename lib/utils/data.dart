92d49c28d008f6f1b27d2088f36e1d98d18e3dc3
baimamboukar <baimamboukar@gmail.com>
2022-02-11 16:03:40 +0100

    adding names in datra

---
+++ lib/utils/data.dart
@@ -0,0 +1,16 @@
+List<String> names = [
+  "ABELA Marceline",
+  "BEDOLO Christopher",
+  "CAMENI William",
+  "MBAS Samuel",
+  "WANTA Georges",
+  "Loremzone Ipsum",
+  "Alpha Beta",
+  "Gamma Delta",
+  "Epsilon Zeta",
+  "Omega Theta",
+  "Iota Sigma",
+  "Kappa Lambda",
+  "Lambda Alpha",
+  "Mu Beta",
+];
