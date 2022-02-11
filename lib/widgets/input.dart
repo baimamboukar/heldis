b0f86d93036532c32bb064f2f5bbf3a3143e2773
baimamboukar <baimamboukar@gmail.com>
2022-02-11 17:05:32 +0100

    input done

--- lib/widgets/input.dart
+++ lib/widgets/input.dart
@@ -9,11 +9,14 @@ class Input extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextFormField(
-      decoration: InputDecoration(
-        labelText: label,
-        hintText: hint,
-        border: const OutlineInputBorder(),
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0, bottom: 20.0),
+      child: TextFormField(
+        decoration: InputDecoration(
+          labelText: label,
+          hintText: hint,
+          border: const OutlineInputBorder(),
+        ),
       ),
     );
   }
