50362742c8bf6bbcb82eeebfa442b094460e2845
baimamboukar <baimamboukar@gmail.com>
2022-02-11 12:55:36 +0100

    adding routename

---
+++ lib/screens/appel.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Appel extends StatelessWidget {
+  const Appel({Key? key}) : super(key: key);
+  static const routeName = '/appel';
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
