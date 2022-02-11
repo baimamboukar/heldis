fa84742c6ab1739616269bb1631222cefc82a4cc
baimamboukar <baimamboukar@gmail.com>
2022-02-11 17:48:16 +0100

    widget created

--- lib/screens/parent_home.dart
+++ lib/screens/parent_home.dart
@@ -1,1 +1,12 @@
 import 'package:flutter/material.dart';
+
+class ParentHome extends StatelessWidget {
+  const ParentHome({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
