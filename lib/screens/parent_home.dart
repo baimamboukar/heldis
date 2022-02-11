b7f08e3f61661b15acc2f75c122407cba89f2a80
baimamboukar <baimamboukar@gmail.com>
2022-02-11 17:54:01 +0100

    parent home route name added

--- lib/screens/parent_home.dart
+++ lib/screens/parent_home.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:heldis/utils/utils.dart';
 
 class ParentHome extends StatelessWidget {
-  const ParentHome({ Key? key }) : super(key: key);
+  const ParentHome({Key? key}) : super(key: key);
+  static const String routeName = '/parent-home';
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
-    );
+    return Scaffold(
+        body: SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        children: [
+          Text(
+            "Cliquez sur le nom d'une personne pour voir ses informations",
+            style:
+                Styles.designText(color: Colors.black87, size: 20, bold: true),
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "L'administration de l'ecole et vous, etes les seules personnes a avoir acces a ces informations",
+            style:
+                Styles.designText(color: Palette.dark, size: 18, bold: false),
+          ),
+        ],
+      ),
+    ));
   }
 }
