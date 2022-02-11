00044ea730592fb4a92b2c3b08655a5828aa07cd
baimamboukar <baimamboukar@gmail.com>
2022-02-11 16:58:11 +0100

    done

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:heldis/utils/utils.dart';
+import 'package:heldis/widgets/button.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({Key? key}) : super(key: key);
@@ -12,15 +13,39 @@ class Welcome extends StatelessWidget {
       physics: const BouncingScrollPhysics(),
       child: Column(
         children: [
-          CircleAvatar(
-            radius: 68.0,
-            backgroundColor: Palette.success,
-            child: Text(
-              "H",
-              style: Styles.designText(
-                  color: Palette.primary, size: 40, bold: true),
+          const SizedBox(height: 90),
+          Center(
+            child: CircleAvatar(
+              radius: 55.0,
+              backgroundColor: Palette.success.withOpacity(0.35),
+              child: Text(
+                "H",
+                style: Styles.designText(
+                    color: Palette.primary, size: 70, bold: true),
+              ),
             ),
-          )
+          ),
+          const SizedBox(height: 30),
+          Text(
+            "Vous etes ?",
+            style: Styles.designText(size: 28, color: Palette.dark, bold: true),
+          ),
+          const SizedBox(height: 30),
+          Button(
+              text: "Parent",
+              callback: () {},
+              withGradient: false,
+              color: Palette.primary),
+          Button(
+              text: "Enseignant",
+              callback: () {},
+              withGradient: false,
+              color: Palette.primary),
+          Button(
+              text: "Ecole",
+              callback: () {},
+              withGradient: false,
+              color: Palette.primary),
         ],
       ),
     ));
