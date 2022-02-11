5ce163106f637da7f18eb3cd9dc7377f4892f5b5
baimamboukar <baimamboukar@gmail.com>
2022-02-11 12:53:41 +0100

    routes url added

--- lib/main.dart
+++ lib/main.dart
@@ -17,29 +17,43 @@ class Heldis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "heldis",
-      home: const Home(),
-      color: const Color.fromARGB(255, 46, 76, 245),
-      theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 32, 9, 238),
-        primarySwatch: const MaterialColor(
-          0xFF0E0062,
-          <int, Color>{
-            50: Color(0xFFA4A4BF),
-            100: Color(0xFFA4A4BF),
-            200: Color(0xFFA4A4BF),
-            300: Color(0xFF9191B3),
-            400: Color(0xFF7F7FA6),
-            500: Color(0xFF181861),
-            600: Color(0xFF6D6D99),
-            700: Color(0xFF5B5B8D),
-            800: Color(0xFF494980),
-            900: Color(0xFF181861),
-          },
+        title: "heldis",
+        home: const Home(),
+        color: const Color.fromARGB(255, 46, 76, 245),
+        theme: ThemeData(
+          primaryColor: const Color.fromARGB(255, 32, 9, 238),
+          primarySwatch: const MaterialColor(
+            0xFF0E0062,
+            <int, Color>{
+              50: Color(0xFFA4A4BF),
+              100: Color(0xFFA4A4BF),
+              200: Color(0xFFA4A4BF),
+              300: Color(0xFF9191B3),
+              400: Color(0xFF7F7FA6),
+              500: Color(0xFF181861),
+              600: Color(0xFF6D6D99),
+              700: Color(0xFF5B5B8D),
+              800: Color(0xFF494980),
+              900: Color(0xFF181861),
+            },
+          ),
+          textTheme: GoogleFonts.openSansTextTheme(),
+          visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
-        textTheme: GoogleFonts.openSansTextTheme(),
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-    );
+        onGenerateRoute: (RouteSettings routeSettings) {
+          return MaterialPageRoute<void>(
+              settings: routeSettings,
+              builder: (BuildContext context) {
+                switch (routeSettings.name) {
+                  case Home.routeName:
+                    return const Home();
+
+                  case Appel.routeName:
+                    return const Appel();
+                  default:
+                    return const Home();
+                }
+              });
+        });
   }
 }
