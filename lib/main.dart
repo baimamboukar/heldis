4374a8959c7a3d1e16016ae8c73a38250837ac0f
baimamboukar <baimamboukar@gmail.com>
2022-02-13 14:10:04 +0100

    arranged splash remove

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:heldis/screens/screens.dart';
 import 'package:heldis/screens/student_details.dart';
 
 Future<void> main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ProviderScope(child: Heldis()));
