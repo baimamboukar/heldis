4448729f65e050df03f82e533f1800fdb290329a
baimamboukar <baimamboukar@gmail.com>
2022-02-13 09:48:43 +0100

    final touch

--- lib/screens/presences.dart
+++ lib/screens/presences.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:heldis/utils/utils.dart';
+import 'package:line_icons/line_icons.dart';
 
 class Presences extends StatelessWidget {
   const Presences({Key? key}) : super(key: key);
@@ -8,6 +10,103 @@ class Presences extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Presences")),
+      body: SingleChildScrollView(
+        child: Column(children: [
+          const SizedBox(
+            height: 20,
+          ),
+          Text(
+            "Aujourd'hui",
+            style:
+                Styles.designText(color: Palette.dark, size: 16, bold: false),
+          ),
+          ...historic,
+          Text(
+            "12/01/2022",
+            style:
+                Styles.designText(color: Palette.dark, size: 16, bold: false),
+          ),
+          ...historic
+        ]),
+      ),
     );
   }
 }
+
+class PresenceLabel extends StatelessWidget {
+  final String type;
+  final String date;
+  final String time;
+
+  final String? message;
+  final bool danger;
+  const PresenceLabel(
+      {Key? key,
+      required this.type,
+      required this.date,
+      required this.time,
+      this.message,
+      required this.danger})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12.0),
+      child: Container(
+        height: 100.0,
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(
+                    LineIcons.user,
+                    color: danger ? Palette.error : Palette.success,
+                    size: 18.0,
+                  ),
+                  Text(
+                    "Arrivee a l'ecole",
+                    style: Styles.designText(
+                        color: danger ? Palette.error : Palette.success,
+                        size: 18.0,
+                        bold: true),
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 20.0,
+              ),
+              Text(
+                message ?? "",
+                style: Styles.designText(
+                    color: Palette.dark, size: 16.0, bold: false),
+              )
+            ],
+          ),
+        ),
+        decoration: BoxDecoration(
+            color: const Color(0xFFF3F5FF),
+            borderRadius: BorderRadius.circular(10.0)),
+      ),
+    );
+  }
+}
+
+List<PresenceLabel> historic = [
+  const PresenceLabel(
+    type: "Arivee a l'ecole",
+    date: "12/01/2022",
+    time: "06h30",
+    message: "Mbas Samuel est arrive a l'ecole a 6h30mins",
+    danger: false,
+  ),
+  const PresenceLabel(
+    type: "Absence en cours",
+    date: "12/01/2022",
+    time: "06h30",
+    message: "Mbas Samuel etait absent au cours de mathematiques a 10H25",
+    danger: true,
+  ),
+];
