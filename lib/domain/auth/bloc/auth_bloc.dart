fc7e54665cbe3e933da4a2844ae037b24e2deb46
baimamboukar <baimamboukar@gmail.com>
2022-03-15 17:24:25 +0100

    auth bloc

---
+++ lib/domain/auth/bloc/auth_bloc.dart
@@ -0,0 +1,11 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'auth_event.dart';
+part 'auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthInitial()) {
+    on<AuthEvent>((event, emit) {});
+  }
+}
